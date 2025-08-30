fd9ba9792ae690016f62af4688f6068f82eea789
Daniel Moreira <danielthooth@gmail.com>
2025-08-30 15:18:06 -0300

    Adiciona o uso de HasFactory no modelo Produto e atualiza o DatabaseSeeder para criar 1000 instâncias de Produto.

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Produto extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['nome', 'preco', 'estoque', 'ativo'];
 }
