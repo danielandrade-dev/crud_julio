7d1fe797368f6abe8f6da69f71408d0dff1e6c8b
Daniel Moreira <danielthooth@gmail.com>
2025-08-30 15:00:13 -0300

    Adiciona rotas para gerenciamento de produtos no arquivo web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,6 +12,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+});
+
+route::prefix('produtos')->group(function () {
+    Route::get('/', [ProdutoController::class, 'index'])->name('produtos.index');
+    Route::post('/', [ProdutoController::class, 'store'])->name('produtos.store');
+    Route::put('/{id}', [ProdutoController::class, 'update'])->name('produtos.update');
+    Route::delete('/{id}', [ProdutoController::class, 'destroy'])->name('produtos.destroy');
 });
 
 require __DIR__.'/settings.php';
